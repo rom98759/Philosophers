5db2dbdafe8c3844c5baa27533e7b7773c1eabf3
rcaillie <rcaillie@student.42angouleme.fr>
2024-12-12 08:52:18 +0100

    parsing int

--- philo/srcs/parsing/parsing.c
+++ philo/srcs/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: rcaillie <rcaillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 13:52:20 by rcaillie          #+#    #+#             */
-/*   Updated: 2024/12/10 15:29:40 by rcaillie         ###   ########.fr       */
+/*   Updated: 2024/12/12 08:51:23 by rcaillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,14 @@ int	is_digit(char *s)
 {
 	int	i;
 
+	i = 0;
 	while (s[i])
 	{
-		if (s)
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
+	return (1);
 }
 
 int	check_args(char *s)
@@ -36,9 +40,9 @@ int	check_args(char *s)
 	if (!s)
 		return (0);
 	n = ft_atol(s);
-	if (!is_digit(n) || !is_valid(n))
+	if (!is_digit(s) || !is_valid(n))
 		return (1);
-
+	return (0);
 }
 
 int	ft_parsing(int nb, char **av)
@@ -48,7 +52,8 @@ int	ft_parsing(int nb, char **av)
 	i = 1;
 	while (i < nb)
 	{
-
+		if (check_args(av[i]))
+			ft_error();
 		else
 			printf("%s\n", av[i]);
 		i++;
--- philo/srcs/philosophers.c
+++ philo/srcs/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: rcaillie <rcaillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 13:38:05 by rcaillie          #+#    #+#             */
-/*   Updated: 2024/12/10 15:24:03 by rcaillie         ###   ########.fr       */
+/*   Updated: 2024/12/12 08:51:54 by rcaillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int main(int ac, char **av)
 {
 	if (!(ac == 5 || ac == 6))
 		return (ft_error(), 1);
-	if (ft_parsing(ac, av))
+	if (!ft_parsing(ac, av))
 		return (1);
-	printf("OK");
+	printf("OK\n");
 	return (0);
 }
